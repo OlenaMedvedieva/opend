21135fe2cd5e8dcd73498a470b7fe4df8e7c58b3
claus <claus18224@gmail.com>
2026-07-24 12:32:18 +0200

    Update NFT frontend

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -3,11 +3,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
 
 function App() {
+
+  const NFTID = "rno2w-sqaaa-aaaaa-aaacq-cai";
+  
   return (
     <div className="App">
       <Header />
+    <Item id={NFTID} />
       <img className="bottom-space" src={homeImage} />
       <Footer />
     </div>
