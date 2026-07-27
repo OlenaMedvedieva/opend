e55cfcfe70784e73f534a321dee353dd3634194c
claus <claus18224@gmail.com>
2026-07-27 12:21:26 +0200

    Complete NFT gallery lesson

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
 import Item from "./Item";
 import Minter from "./Minter";
 
 function App() {
 
-  const NFTID = "rno2w-sqaaa-aaaaa-aaacq-cai";
+  //const NFTID = "rno2w-sqaaa-aaaaa-aaacq-cai";
   
   return (
     <div className="App">
       <Header />
-      <Minter />
-
-  {/*<Item id={NFTID} />*/}
-   {/*   <img className="bottom-space" src={homeImage} /> */}
+     {/*<Minter /> */}
+     {/*<Item id={NFTID} />*/}
       <Footer />
     </div>
   );
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,50 +1,76 @@
+
 import React from "react";
-import logo from "../../assets/logo.png";
-import {Actor, HttpAgent} from "@dfinity/agent";
-import {idlFactory} from "../../../declarations/nft";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
 import { Principal } from "@dfinity/principal";
 
-
 function Item(props) {
-
   const [name, setName] = React.useState("");
   const [owner, setOwner] = React.useState("");
   const [image, setImage] = React.useState("");
 
-  const id = Principal.fromText( props.id);
+  
+async function loadNFT() {
+  try {
 
+    const id = props.id;
 
-const localhost = "http://localhost:8080";
-  const agent = new HttpAgent({ host: localhost});
-
-  async function loadNFT() {
-    const NFTActor = await Actor.createActor(idlFactory, {  
-      agent,
-      canisterId:id , 
+    const agent = new HttpAgent({
+      host: "http://127.0.0.1:8000",
     });
 
-    const name = await NFTActor.getName();
-    const owner = await NFTActor.getOwner();
+    await agent.fetchRootKey();
+
+    const NFTActor = Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+
+    const nftName = await NFTActor.getName();
+    const nftOwner = await NFTActor.getOwner();
     const imageData = await NFTActor.getAsset();
+
+    console.log("NFT name:", nftName);
+    console.log("NFT owner:", nftOwner.toText());
+    console.log("Image bytes:", imageData.length);
+
     const imageContent = new Uint8Array(imageData);
-    const image = URL.createObjectURL(new Blob([imageContent.buffer], { type: "image/png" }));
-    setName(name);
-    setOwner(owner.toText());
-    setImage(image);
+
+    const imageUrl = URL.createObjectURL(
+      new Blob([imageContent], {
+        type: "image/png",
+      })
+    );
+
+    setName(nftName);
+    setOwner(nftOwner.toText());
+    setImage(imageUrl);
+  } catch (error) {
+    console.error("NFT loading error:", error);
   }
+}
 
-    React.useEffect(() => {
+
+  React.useEffect(() => {
+    if (props.id) {
       loadNFT();
-    }, []);
-
+    }
+  }, [props.id]);
 
   return (
-      <div>
-    <h1>{name}</h1>
-    <p>{owner}</p>
-    <img src={image} />
-  </div>
-);
+    <div>
+      <h1>{name}</h1>
+      <p>{owner}</p>
 
+      {image && (
+        <img
+          src={image}
+          alt={name}
+        />
+      )}
+    </div>
+  );
 }
+
 export default Item;
+
